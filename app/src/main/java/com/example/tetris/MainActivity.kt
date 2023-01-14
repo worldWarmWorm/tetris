fbf03d21a47d5de68eabda03b52cb62e153c6aa0
worldwarmworm <st.valeriq@gmail.com>
2023-01-14 17:54:03 +0700

    Create app

---
+++ app/src/main/java/com/example/tetris/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.tetris
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    var tvHighScore: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        val btnNewGame = findViewById<Button>(R.id.btn_new_game)
+        val btnResetScore = findViewById<Button>(R.id.btn_reset_score)
+        val btnExit = findViewById<Button>(R.id.btn_exit)
+        tvHighScore = findViewById(R.id.tv_high_score)
+
+        btnNewGame.setOnClickListener(this::onBtnNewGameClick)
+        btnResetScore.setOnClickListener(this::onBtnResetScore)
+        btnExit.setOnClickListener(this::onBtnExitClick)
+    }
+
+    private fun onBtnNewGameClick(view: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun onBtnResetScore(view: View) {
+
+    }
+
+    private fun onBtnExitClick(view: View) {
+        System.exit(0)
+    }
+}
