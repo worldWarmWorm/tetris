64e124865f31ca2f2fca437a24e29688c5d39c2f
worldwarmworm <st.valeriq@gmail.com>
2023-01-18 22:00:15 +0700

    WIP

--- app/src/main/java/com/example/tetris/GameActivity.kt
+++ app/src/main/java/com/example/tetris/GameActivity.kt
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class GameActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_game)
+	}
 }
