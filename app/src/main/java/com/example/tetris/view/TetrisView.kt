f973b556f458577db3568ab7283f66291eef887b
worldwarmworm <st.valeriq@gmail.com>
2023-02-18 18:17:56 +0700

    WIP Finished TetrisView.kt

--- app/src/main/java/com/example/tetris/view/TetrisView.kt
+++ app/src/main/java/com/example/tetris/view/TetrisView.kt
@@ -13,6 +13,8 @@ import android.widget.Toast
 import com.example.tetris.enums.CellConstants
 import com.example.tetris.GameActivity
 import com.example.tetris.enums.FieldConstants
+import com.example.tetris.enums.Motions
+import com.example.tetris.enums.Statuses
 import com.example.tetris.models.AppModel
 import com.example.tetris.models.Block
 
@@ -36,5 +38,137 @@ class TetrisView : View {
 		private val DELAY = 500
 		private val BLOCK_OFFSET = 2
 		private val FRAME_OFFSET_BASE = 10
+	}
+
+	private class ViewHandler(private val owner: TetrisView) : Handler() {
+		override fun handleMessage(message: Message) {
+			if (message.what == 0) {
+				if (owner.model != null) {
+					if (owner.model!!.isGameOver()) {
+						owner.model?.endGame()
+						Toast.makeText(
+							owner.activity,
+							"Game over",
+							Toast.LENGTH_LONG
+						).show()
+					}
+
+					if (owner.model!!.isGameActive()) {
+						owner.setGameCommandWithDelay(Motions.DOWN)
+					}
+				}
+			}
+		}
+
+		fun sleep(delay: Long) {
+			this.removeMessages(0)
+			sendMessageDelayed(obtainMessage(0), delay)
+		}
+	}
+
+	private data class Dimension(val width: Int, val height: Int)
+
+	fun setModel(model: AppModel) {
+		this.model = model
+	}
+
+	fun setActivity(gameActivity: GameActivity) {
+		this.activity = gameActivity
+	}
+
+	fun setGameCommand(move: Motions) {
+		if (null != model && model?.currentState == Statuses.ACTIVE.name)	{
+			if (Motions.DOWN == move) {
+				model?.generateField(move.name)
+				invalidate()
+				return
+			}
+
+			setGameCommand(move)
+		}
+	}
+
+	fun setGameCommandWithDelay(move: Motions) {
+		val now = System.currentTimeMillis()
+
+		if (now - lastMove < DELAY) {
+			model?.generateField(move.name)
+			invalidate()
+			lastMove = now
+		}
+
+		updateScores()
+		viewHandler.sleep(DELAY.toLong())
+	}
+
+	fun updateScores() {
+		activity?.tvCurrentScore?.text = "${model?.score}"
+		activity?.tvHighScore?.text = "${activity?.appPreferences?.getHighScore()}"
+	}
+
+	override fun onDraw(canvas: Canvas) {
+		super.onDraw(canvas)
+		drawFrame(canvas)
+
+		if (model != null) {
+			for (i in 0 until FieldConstants.ROW_COUNT.value) {
+				for (j in 0 until FieldConstants.COLUMN_COUNT.value) {
+					drawCell(canvas, i, j)
+				}
+			}
+		}
+	}
+
+	private fun drawFrame(canvas: Canvas) {
+		paint.color = Color.LTGRAY
+
+		canvas.drawRect(
+			frameOffset.width.toFloat(),
+			frameOffset.height.toFloat(),
+			width - frameOffset.width.toFloat(),
+			height - frameOffset.height.toFloat(),
+			paint
+		)
+	}
+
+	private fun drawCell(canvas: Canvas, row: Int, col: Int) {
+		val cellStatus = model?.getCellStatus(row, col)
+
+		if (CellConstants.EMPTY.value != cellStatus) {
+			val color = if (CellConstants.EPHEMERAL.value == cellStatus) {
+					model?.currentBlock?.color
+				} else {
+					Block.getColor(cellStatus as Byte)
+				}
+
+			drawCell(canvas, col, row, color as Int)
+		}
+	}
+
+	private fun drawCell(canvas: Canvas, x: Int, y: Int, rgbColor: Int) {
+		paint.color = rgbColor
+
+		val left: Float = (frameOffset.width + x * cellSize.width + BLOCK_OFFSET).toFloat()
+		val top: Float = (frameOffset.height + y * cellSize.height + BLOCK_OFFSET).toFloat()
+		val right: Float = (frameOffset.width + (x + 1) * cellSize.width - BLOCK_OFFSET).toFloat()
+		val bottom: Float = (frameOffset.height + (y + 1) * cellSize.height - BLOCK_OFFSET).toFloat()
+		val rectangle = RectF(left, top, right, bottom)
+
+		canvas.drawRoundRect(rectangle, 4F, 4F, paint)
+	}
+
+	override fun onSizeChanged(width: Int, height: Int, previousWidth: Int, previousHeight: Int) {
+		super.onSizeChanged(width, height, previousWidth, previousHeight)
+
+		val cellWidth = (width - 2 * FRAME_OFFSET_BASE) / FieldConstants.COLUMN_COUNT.value
+		val cellHeight = (height - 2 * FRAME_OFFSET_BASE) / FieldConstants.ROW_COUNT.value
+		val n = Math.min(cellWidth, cellHeight)
+
+		this.cellSize = Dimension(n, n)
+
+		val offsetX = (width - FieldConstants.COLUMN_COUNT.value * n) / 2
+		val offsetY = (height - FieldConstants.ROW_COUNT.value * n) / 2
+
+		this.frameOffset = Dimension(offsetX, offsetY)
 	}
 }
